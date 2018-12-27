a8eb57471b0c40e32e84d8601efe3de63fadb00c
Afsalms <afsalms93.mec@gmail.com>
2018-12-27 19:20:24 +0530

    Small change in process reposnse

--- app/src/main/java/com/example/qbuser/countrylist/retrofit/ProcessResponse.kt
+++ app/src/main/java/com/example/qbuser/countrylist/retrofit/ProcessResponse.kt
@@ -1,27 +1,66 @@
 package com.example.qbuser.countrylist.retrofit
-import com.example.qbuser.countrylist.adapators.CountryAdaptor
 import com.example.qbuser.countrylist.models.CountryResponse
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
-class ProcessResponse(var retrofitCallObj: Call<Any>){
+open class SimpleResponse {
+    var status: Int = 0
+    var message: String? = null
+    fun isSuccess() = status==200
+}
 
-    var t1:Throwable? = null
-    var r:Response<Any>? = null
+sealed class ApiResult<out T: Any>
+data class Success<out T: Any>(val contents: T): ApiResult<T>()
+data class Failure(val errorCode: Int, val throwable: Throwable): ApiResult<Nothing>()
 
-    fun getResponse(){
-        retrofitCallObj.enqueue(object : Callback<Any>{
-            override fun onFailure(call: Call<Any>, t: Throwable) {
-                t1 = t
-            }
 
-            override fun onResponse(call: Call<Any>, response: Response<Any>) {
-                r = response
+class DhowberApiHelper(){
+    val retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl("http://dhowberbackend.qburst.build/")
+        .build()
+    val retrofitServiceObj = retrofit.create(RetrofitService::class.java)
 
-            }
-        })
+
+    fun getCountryDetails(): ApiResult<CountryResponse> = retrofitServiceObj.getAllCountries().ProcessResponse<CountryResponse>()
+
+}
+
+
+
+fun <T: Any>Call<T>.ProcessResponse(): ApiResult<T>{
+    return try {
+
+        val response = execute()
+
+        if(response.isSuccessful && response.body() != null){
+
+            Success(response.body() as T)
+
+        } else {
+
+            val errorBody = response.errorBody()?.string()
+            println("*******************************************************")
+            println(errorBody)
+            println("................................................................")
+            val errorResponse = Gson().fromJson(errorBody, SimpleResponse::class.java)
+
+            val errorMessage = errorResponse?.message ?: "Code:${response.code()} - Empty response."
+            Failure(response.code(), Throwable(errorMessage))
+        }
+
+    } catch (e: Exception) {
+        println("++++++++++++++++++++++++++++++++++++++++++++++++")
+        println(e)
+        println("-------------------------------------------------------")
+        Failure(-1, Throwable(e.localizedMessage))
     }
+
+
+
 }
